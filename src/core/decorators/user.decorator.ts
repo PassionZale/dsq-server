e3337a00baf574cffe03b7e7a818d83e51e7244f
Zhang Lei <372470745@qq.com>
2023-01-09 19:14:07 +0800

    wip: decorator

---
+++ src/core/decorators/user.decorator.ts
@@ -0,0 +1,15 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Omit, ValuesType } from 'utility-types';
+import { UserEntity } from '@/features/user/user.entity';
+
+export type K = keyof Omit<UserEntity, 'hashed_password'>;
+export type V = ValuesType<UserEntity>;
+
+export const User = createParamDecorator(
+  (data: K | undefined, ctx: ExecutionContext): V | UserEntity => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user as UserEntity;
+
+    return data ? user[data] : user;
+  },
+);
